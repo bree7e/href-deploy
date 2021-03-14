a47ca62c3ccad04a926418559a39c36f6fe58819
Alexandr Vetrov <vetrov@icc.ru>
2021-03-14 22:31:39 +0800

    feat: add dynamic BASE_HREF & DEPLOY_URL

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -Eeo pipefail
-sed -i "s|__API_URL__|${API_URL}|"  /usr/share/nginx/html/index.html
-# envsubst < "src/index.env.html" > "src/index.html"
+sed -i -e "s|__BASE_HREF__|"${BASE_HREF:-/}"|" -e "s|__DEPLOY_URL__|"${DEPLOY_URL:-''}"|g" /usr/share/nginx/html/index.html
 cat /usr/share/nginx/html/index.html
 exec "$@"
