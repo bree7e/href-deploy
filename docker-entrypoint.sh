6213a07bb7025c8513301090711aef55a752870b
Alexandr Vetrov <vetrov@icc.ru>
2021-03-14 15:50:58 +0800

    docs: add docker-compose file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -Eeo pipefail
-# sed -i "s|__API_URL__|${__API_URL__}|"  /usr/share/nginx/html/index.html
-envsubst < "src/index.env.html" > "src/index.html"
+sed -i "s|__API_URL__|${API_URL}|"  /usr/share/nginx/html/index.html
+# envsubst < "src/index.env.html" > "src/index.html"
 cat /usr/share/nginx/html/index.html
 exec "$@"
